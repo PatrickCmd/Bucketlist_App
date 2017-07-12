ea1967665c1c3843caaa6da09c4fb3b711c1d022
Patrick .Py <pwalukagga@gmail.com>
2017-07-12 19:14:10 +0300

    patched the user class

--- bucket_app/classes/user.py
+++ bucket_app/classes/user.py
@@ -14,3 +14,20 @@ class User(object):
     def create_user_bucketlist(self, bucketlist):
         self.buckets.append(bucketlist)
     
+    def view_user_bucketlist(self, user_id):
+        pass
+    
+    def edit_user_bucketlist(self, user_id):
+        pass
+    
+    def delete_user_bucketlist(self, user_id):
+        pass
+    
+    def create_bucketlist_item(self, bucketname):
+        pass
+    
+    def edit_bucketlist_item(self, bucketname):
+        pass
+    
+    def delete_bucketlist_item(self, bucketname):
+        pass
