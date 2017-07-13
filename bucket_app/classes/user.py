e32f6e405ca16cd47265a7f4d77c4a65c466363f
Patrick .Py <pwalukagga@gmail.com>
2017-07-13 20:37:12 +0300

    modified user class

--- bucket_app/classes/user.py
+++ bucket_app/classes/user.py
@@ -17,17 +17,24 @@ class User(object):
     def view_user_bucketlist(self, user):
         pass
     
-    def edit_user_bucketlist(self, user_id):
-        pass
+    def edit_user_bucketlist(self, bucket_name, new_bucket_name, bucket_description):
+        '''Editing user buckets'''
+        for bucket in self.buckets:
+            if bucket.name == bucket_name:
+                bucket.name = new_bucket_name
+                bucket.description = bucket_description
     
-    def delete_user_bucketlist(self, bucketlist):
-        self.buckets.remove(bucketlist)
+    def delete_user_bucketlist(self, bucket_name):
+        '''Delete user bucket'''
+        for bucket in self.buckets:
+            if bucket.name == bucket_name:
+                self.buckets.remove(bucket)
     
-    def create_bucketlist_item(self, bucketname, bucketitem):
+    def create_bucketlist_item(self, bucket_name, bucketitem):
         '''creates bucketlist items for the user buckets'''
         for bucket in self.buckets:
-            if bucket.name == bucketname:
-                bucket.bucketitems.append[bucketitem]
+            if bucket.name == bucket_name:
+                bucket.bucketitems.append(bucketitem)
     
     def edit_bucketlist_item(self, bucketname):
         pass
