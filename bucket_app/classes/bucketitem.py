6e830dfe87af885ce4caec0ac3aeb14499a2df94
Patrick .Py <pwalukagga@gmail.com>
2017-07-13 14:04:01 +0300

    created class for bucket item

---
+++ bucket_app/classes/bucketitem.py
@@ -0,0 +1,8 @@
+class BucketItem(object):
+    '''Class to create bucketitem objects'''
+
+    def __init__(self, name=None, description=None):
+        self.name = name
+        self.description = description
+        # self.bucketitems = []
+
