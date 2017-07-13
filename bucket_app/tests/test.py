431bfa22bee2d4708cd1d12f5a78bfe0e537791c
Patrick .Py <pwalukagga@gmail.com>
2017-07-13 05:00:07 +0300

    test for index route

--- bucket_app/tests/test.py
+++ bucket_app/tests/test.py
@@ -1,3 +1,4 @@
+from app import app
 import unittest
 from classes.bucketapp import BucketApp
 from classes.user import User
@@ -8,10 +9,17 @@ class CrudTests(unittest.TestCase):
     '''Tests for the crud functionalities of the application'''
 
     def setUp(self):
+        
+        # creating testclient for the application
+        self.app = app.test_client()
+        self.app.testing = True
+
+
         self.bucketapp = BucketApp()
         self.user = User('pat@gmail.com', 'pat123')
         self.bucket = Bucket()
 
+    # crud operation tests    
     def test_user_is_created_successfully(self):
         self.bucketapp.registerUser(self.bucketapp)
         self.assertIsInstance(self.bucketapp.users_created, list)
@@ -21,3 +29,13 @@ class CrudTests(unittest.TestCase):
     
     def test_bucketlist_item_is_created(self):
         self.assertIsInstance(self.bucket.bucketitems, list)
+
+    # route tests
+    def test_home_status_code(self):
+        '''sends HTTP GET request to the application on the
+        specified'''
+
+        # send request
+        result = self.app.get('/')
+        # asserting the status code of the response
+        self.assertEqual(result.status_code, 200)
